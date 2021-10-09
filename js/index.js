27f173ea7e5e6ebc57d828e8798c23cc1af6a979
Om Kakatkar <omkakatkar@gmail.com>
2021-10-09 20:16:22 +0530

    fix: form acceptiong negative values

--- js/index.js
+++ js/index.js
@@ -14,39 +14,44 @@ check.addEventListener("click", () => {
 });
 
 function clickHandler(initial, qty, final, op) {
+  // Default behaviour of GiF and color
   happyGif.style.display = "none";
   sadGif.style.display = "none";
   main.style.backgroundColor = "rgb(245, 248, 70)";
+
   if (initial !== "" && qty !== "" && final !== "") {
-    initial = parseFloat(initial);
-    qty = parseFloat(qty);
-    final = parseFloat(final);
-    let costPrice = initial * qty;
-    let sellPrice = final * qty;
-    if (costPrice < sellPrice) {
-      let profit = sellPrice - costPrice;
-      let profitPercent = ((profit / costPrice) * 100).toFixed(2);
-      happyGif.style.display = "block";
-      output.textContent = `Yay you just earned an absolute profit of ₹${profit} which is ${profitPercent}% of the initial price`;
-      if (parseFloat(profitPercent) >= 50) {
-        main.style.backgroundColor = "green";
-      }
-    } else if (costPrice > sellPrice) {
-      let loss = costPrice - sellPrice;
-      let lossPercent = ((loss / costPrice) * 100).toFixed(2);
-      sadGif.style.display = "block";
-      output.textContent = `Aww you just lost ₹${loss} which is ${lossPercent}% of the initial price`;
-      if (parseFloat(lossPercent) >= 50) {
-        main.style.backgroundColor = "red";
+    if (initial >= 0 && qty >= 0 && final >= 0) {
+      initial = parseFloat(initial);
+      qty = parseFloat(qty);
+      final = parseFloat(final);
+      let costPrice = initial * qty;
+      let sellPrice = final * qty;
+      if (costPrice < sellPrice) {
+        let profit = sellPrice - costPrice;
+        let profitPercent = ((profit / costPrice) * 100).toFixed(2);
+        happyGif.style.display = "block";
+        output.textContent = `Yay you just earned an absolute profit of ₹${profit} which is ${profitPercent}% of the initial price`;
+        if (parseFloat(profitPercent) >= 50) {
+          main.style.backgroundColor = "green";
+        }
+      } else if (costPrice > sellPrice) {
+        let loss = costPrice - sellPrice;
+        let lossPercent = ((loss / costPrice) * 100).toFixed(2);
+        sadGif.style.display = "block";
+        output.textContent = `Aww you just lost ₹${loss} which is ${lossPercent}% of the initial price`;
+        if (parseFloat(lossPercent) >= 50) {
+          main.style.backgroundColor = "red";
+        }
+      } else {
+        console.log("No profit no loss");
+        output.textContent = "No profit no loss";
       }
     } else {
-      console.log("No profit no loss");
-      output.textContent = "No profit no loss";
+      console.log("Positive Amount Only");
+      output.textContent = "Positive Amount Only";
     }
   } else {
     console.log("Please enter all details");
     output.textContent = "Please enter all details";
   }
 }
-
-// TODO: Add proper UI
